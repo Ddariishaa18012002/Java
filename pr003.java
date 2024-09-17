f3b9dbfb42d6c1bcb2f2be85a23245f8742baef1
Ddariishaa18012002 <dasha.perova.02@inbox.ru>
2024-09-17 14:29:03 +0700

    Добавили программу чтения символов с клавиатуры

--- pr003.java
+++ pr003.java
@@ -1,9 +1,18 @@
-class pr003 {
+class pr_trilogy {
 	public static void main(String args[]) throws java.io.IOException {
+
 		//Чтение с клавиатуры
 		char ch;
 		System.out.print("Нажмите клавишу на клавиатуре и ENTER: ");
 		ch = (char) System.in.read();
 		System.out.println("Вы нажали клавишу: " + ch);
+
+                //Угадывание буквы
+		//Пример использования проверки условий
+		char answer = 'r';
+		if (ch == answer)
+			System.out.println("Буква указана верно");
+		else
+			System.out.println("Неверная буква");
         }
 }	
