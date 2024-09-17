cacc38ed08d6e58bc2255a0292cf62ba28f2af25
Ddariishaa18012002 <dasha.perova.02@inbox.ru>
2024-09-17 13:52:28 +0700

    Добавлены условный оператор т оператор цикла

--- pr001.java
+++ pr001.java
@@ -11,7 +11,23 @@ class pr001 {
 		System.out.println("Результат деления приведением типа данных: " + (double)Var2345678/2);
 
 		double Var3 = (double)Var2345678/2;
+
+		//Пример использования оператора if
+		//if(Var1 = 0)
+		//      System.out.println("Условие Var1=0 выполнено"); Несовместимость типов в условии
+                if(Var1/2 == 512)
+			System.out.println("Условие \" Var1/2==512\" выполнено");
+		if(true)
+		System.out.println("Строка для проверки");
+
+		//Пример использования оператора for
+		for (int i=0; i<10; i++)
+			System.out.println("Строка номер " + i);
+		//i = 10; Переменная i локальная и существует только во время выполнения цикла
+                System.out.println();
+		int j;
+		for (j=0; j<10; j++)
+			System.out.println(j + " ");
+		System.out.println();
 	}
-}
-
-
+}	
