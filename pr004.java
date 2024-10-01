b5b9c9f3d1017cbe1a09fe2ab24edd2f532faf1e
Ddariishaa18012002 <dasha.perova.02@inbox.ru>
2024-10-01 15:03:03 +0700

    Объявили класс и создали экземпляр класса

---
+++ pr004.java
@@ -0,0 +1,23 @@
+class Avtomobil {
+	int pass; //количество пассажиров
+	int v; //объём топливного бака в л.
+	double rash; //расход топлива в л. на 100 км.
+}
+class pr004 {
+	public static void main(String args[]) {
+		//Создание экземпляра класса
+	        Avtomobil lada = new Avtomobil();
+	        int rasst;
+
+	        //Присваивание значения внутренним переменным экземпляра класса
+	        lada.pass = 4;
+	        lada.v = 40;
+	        lada.rash = 9.5;
+                
+		//Расчет расстояния, которое lada проедет на полном баке
+		rasst = (int) (lada.v/lada.rash * 100);
+
+		System.out.println("Lada проедет " + rasst + " км. на полном баке");
+        }
+}
+     
